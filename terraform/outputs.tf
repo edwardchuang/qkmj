199c6a7005de8849b552b09ae8d11d9f51a1f6de
Edward Chuang <edwardchuang@gmail.com>
2025-12-24 15:21:57 +0800

    feat(infra): Add terraform outputs for server IP and registry

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
 output "server_ip" {
-  description = "The public static IP address of the game server"
+  description = "The public IP address of the QKMJ server"
   value       = google_compute_address.static_ip.address
+}
+
+output "artifact_registry_repo" {
+  description = "The Artifact Registry repository name"
+  value       = google_artifact_registry_repository.qkmj_repo.name
 }
